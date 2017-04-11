28b21fd583405ff2b88a63bc14850f856fb67dfe
邱国辉 <530784837@qq.com>
2017-04-11 17:59:23 +0800

    完成基本爬虫功能

---
+++ config/index.js
@@ -0,0 +1,6 @@
+var config = {
+  url: 'https://www.baidu.com/',
+  // url: 'https://s.1688.com/selloffer/offer_search.htm?keywords=%CD%E2%C3%B3&n=y&spm=a260k.635.1998096057.d1',
+  ulSelector: '#sm-offer-list'
+}
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+var config = require('../config/index')
+var cheerio = require('cheerio')
+var url = require('url')
+
+var options = {
+  url: config.url
+};
+
+var $
+var content = ''
+var page
+var selector = '#u1 a'
+var rex = /^http:\/\//
+var cnodeUrl = config.url
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  request(options, function (error, response, body) {
+    if (error) {
+      return console.log(error)
+    }
+    $ = cheerio.load(body)
+    var shopUrls = []
+    $(selector).each(function (index, element) {
+      var $element = $(element)
+      var href = url.resolve(cnodeUrl, $element.attr('href'));
+      shopUrls.push(href)
+    })
+    // var lists = $(selector)
+    // for (var i = 0; i < lists.length; i++) {
+    //   var href = lists[i].attribs.href
+    //   if (rex.test(href)) {
+    //     shopUrls.push(lists[i].attribs.href)
+    //   }
+    // }
+
+    content = shopUrls
+    render(res, content)
+  })
 });
+
+function render (res, jsonContent) {
+  res.send(jsonContent)
+}
 
 module.exports = router;
